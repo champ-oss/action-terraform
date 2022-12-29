c17bdb86b9a3f9e2ab9d462fe26541a369d4ae5f
Jeremiah Norris <jjno91@gmail.com>
2022-12-29 15:49:31 -0600

    find bucket by prefix

--- main.py
+++ main.py
@@ -1,4 +1,46 @@
+import logging
+
+import boto3
+
+
 def connect_backend(prefix: str = 'terraform-backend'):
+    bucket = find_bucket(prefix)
+
+    if bucket == '':
+        bucket = create_bucket(prefix)
+
+    return create_backend(bucket)
+
+
+def find_bucket(prefix: str = 'terraform-backend'):
+    s3 = boto3.client('s3')
+    response = s3.list_buckets()
+    matched_buckets = []
+
+    logging.debug(response)
+
+    for bucket in response['Buckets']:
+        if bucket["Name"].startswith(prefix):
+            matched_buckets.append(bucket)
+
+    if len(matched_buckets) < 1:
+        print('No bucket found')
+        return ''
+
+    if len(matched_buckets) > 1:
+        print('Multiple buckets found: ', matched_buckets)
+        return ''
+
+    return matched_buckets.pop()
+
+
+def create_bucket(prefix: str = 'terraform-backend'):
+    bucket = ''
+    print('create bucket')
+    return bucket
+
+
+def create_backend(bucket: str = 'terraform-backend'):
     return
 
 
